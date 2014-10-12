e1cd6d0fb622115d5243eab4c98e265a03a99e8b
spline <reticulatingspline@github.com>
2014-10-12 16:45:54 -0400

    Add in two tests.

--- test.py
+++ test.py
@@ -9,6 +9,11 @@ from supybot.test import *
 
 class ZNCTestCase(PluginTestCase):
     plugins = ('ZNC',)
+    
+    def testZNC(self):
+        # zncadduser, znclistusers, zncprintcache, and zncremoveuser
+        self.assertResponse("zncadduser spline *!*spline@*host.mask key #test", "The operation succeeded.")
+        self.assertResponse("zncremoveuser spline", "The operation succeeded.")
 
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
